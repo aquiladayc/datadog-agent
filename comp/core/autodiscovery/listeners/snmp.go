8129901ce50c6cffb0fede571b25ed787e9f3023
Jean-Edouard Dupau <jeanedouard.dupau@datadoghq.com>
2025-04-07 14:31:08 +0200

    fix autodiscoverys tatus

--- comp/core/autodiscovery/listeners/snmp.go
+++ comp/core/autodiscovery/listeners/snmp.go
@@ -506,7 +506,7 @@ func (l *SNMPListener) checkDevices() {
 		for i := range subnets {
 			// Use `&subnets[i]` to pass the correct pointer address to snmpJob{}
 			subnet = &subnets[i]
-			subnet.devicesScannedCounter.Store(0)
+			subnet.devicesScannedCounter.Store(uint32(len(subnet.config.IgnoredIPAddresses)))
 
 			startingIP := make(net.IP, len(subnet.startingIP))
 			copy(startingIP, subnet.startingIP)
